ddecb3d07c90e220e31876e2aebc6b13906f0641
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-05-10 20:55:31 +0400

    commit-025 - update tests

--- tests/test_train.py
+++ tests/test_train.py
@@ -1,4 +1,6 @@
-
+from click.testing import CliRunner  # type: ignore
+from evaluation_selection_project.train import train  # type: ignore
+import pytest  # type: ignore
 from click.testing import CliRunner
 import pytest
 
@@ -14,13 +16,25 @@ def runner() -> CliRunner:
 def test_error_for_invalid_test_split_ratio(
     runner: CliRunner
 ) -> None:
-    """It fails when test split ratio is greater than 1."""
+    """It fails when test split ratio is greater than 10"""
     result = runner.invoke(
         train,
         [
             "--test-split-ratio",
-            42,
+            33,
         ],
     )
     assert result.exit_code == 2
     assert "Invalid value for '--test-split-ratio'" in result.output
+
+
+def test_error_for_invalid_test_split_ratio(runner: CliRunner) -> None:
+    """It fails when test split ratio is less than 0."""
+    result = runner.invoke(
+        train,
+        [
+            "--test-split-ratio",
+            "-10",
+        ],
+    )
+    assert result.exit_code == 1
