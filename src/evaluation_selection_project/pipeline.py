2264e31d8a13f5fb06bfd1cbb5e6407637ef755c
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-05-10 19:55:39 +0400

    commit-022 - add accuracy, deleted grid (((

--- src/evaluation_selection_project/pipeline.py
+++ src/evaluation_selection_project/pipeline.py
@@ -10,7 +10,7 @@ def create_pipeline_Logistic_Regression(
     max_iter: int, 
     logreg_C: float,
     with_feature_selection: int,
-    with_grid: bool,
+    # with_grid: bool,
     random_state: int,
 ) -> Pipeline:
     pipeline_steps = []
@@ -39,21 +39,21 @@ def create_pipeline_Logistic_Regression(
             )
         )
 
-    if with_grid is True:
-        pipeline_steps.append(
-            (
-                "classifier",
-                LogisticRegression(random_state=random_state),
-            )
-        )
+    # if with_grid is True:
+    #     pipeline_steps.append(
+    #         (
+    #             "classifier",
+    #             LogisticRegression(random_state=random_state),
+    #         )
+    #     )
 
-    if with_grid is False:
-        pipeline_steps.append(
-            (
-                "classifier",
-                LogisticRegression(random_state=random_state, max_iter=max_iter, C=logreg_C),
-            )
+    # if with_grid is False:
+    pipeline_steps.append(
+        (
+            "classifier",
+            LogisticRegression(random_state=random_state, max_iter=max_iter, C=logreg_C),
         )
+    )
 
     return Pipeline(steps=pipeline_steps)
 
@@ -62,7 +62,7 @@ def create_pipeline_RandomForest(
     with_scaler: bool,
     n_estimators: int,
     with_feature_selection: int,
-    grid_search: bool,
+    # grid_search: bool,
     random_state: int,
 ) -> Pipeline:
     pipeline_steps = []
@@ -91,19 +91,20 @@ def create_pipeline_RandomForest(
             )
         )
 
-    if grid_search is False:
-        pipeline_steps.append(
-            (
-                "classifier",
-                RandomForestClassifier(n_estimators=n_estimators, random_state=random_state),
-            )
+    # if grid_search is False:
+    pipeline_steps.append(
+        (
+            "classifier",
+            RandomForestClassifier(n_estimators=n_estimators, random_state=random_state),
         )
+    )
         
-    if grid_search is True:
-        pipeline_steps.append(
-            (
-                "classifier",
-                RandomForestClassifier(random_state=random_state),
-            )
-        )
+    # if grid_search is True:
+    #     pipeline_steps.append(
+    #         (
+    #             "classifier",
+    #             RandomForestClassifier(random_state=random_state),
+    #         )
+    #     )
+    
     return Pipeline(steps=pipeline_steps)
--- src/evaluation_selection_project/train.py
+++ src/evaluation_selection_project/train.py
@@ -5,6 +5,7 @@ import click
 import mlflow  # type: ignore[unused-ignore]
 import mlflow.sklearn  # type: ignore[unused-ignore]
 
+from sklearn.metrics import accuracy_score  # type: ignore[unused-ignore]
 from sklearn.model_selection import KFold  # type: ignore[unused-ignore]
 from sklearn.model_selection import GridSearchCV  # type: ignore[unused-ignore]
 from sklearn.model_selection import cross_val_score  # type: ignore[unused-ignore]
@@ -49,7 +50,7 @@ from .pipeline import create_pipeline_RandomForest
 
 @click.option(
     "--max-iter",
-    default=1000,
+    default=200,
     type=int,
 )
 
@@ -64,10 +65,10 @@ from .pipeline import create_pipeline_RandomForest
     default=0, 
     type=int)
 
-@click.option(
-    "--with-grid",
-    default=False,
-    type=bool)
+# @click.option(
+#     "--with-grid",
+#     default=True,
+#     type=bool)
 
 @click.option(
     "--model-selector", 
@@ -90,7 +91,7 @@ def train(
     logreg_c: float,
     with_feature_selection: int,
     model_selector: int,
-    with_grid: bool,
+    # with_grid: bool,
     n_estimators: int,
 ) -> None:
     features, target = get_dataset(
@@ -110,46 +111,46 @@ def train(
 
             mlflow.log_param("Selected Model", "Logistic Regression")
 
-            if with_grid is False:
-                mlflow.log_param("max_iter", max_iter)
-                mlflow.log_param("logreg_c", logreg_c)
+            # if with_grid is False:
+            mlflow.log_param("max_iter", max_iter)
+            mlflow.log_param("logreg_c", logreg_c)
 
-            if with_grid is True:
-                cv_inner = KFold(n_splits=3, shuffle=True, random_state=1)
-                param_grid = {
-                    "penalty": ["l1", "l2"],
-                    "C": [1, 10, 100],
-                    "solver": ["newton-cg", "lbfgs", "liblinear"],
-                    "max_iter": [100, 200, 300, 500],
-                }
-                scoring = [
-                    "accuracy",
-                    "f1_weighted",
-                    "precision_weighted",
-                    "recall_weighted",
-                ]
-                search = GridSearchCV(
-                    pipeline,
-                    param_grid=param_grid,
-                    n_jobs=1,
-                    cv=cv_inner,
-                    scoring=scoring,
-                    refit="f1_weighted",
-                )
+            # if with_grid is True:
+            #     cv_inner = KFold(n_splits=3, shuffle=True, random_state=1)
+            #     param_grid = {
+            #         "penalty": ["l1", "l2"],
+            #         "C": [1, 10, 20, 50],
+            #         "solver": ["newton-cg", "lbfgs", "liblinear"],
+            #         "max_iter": [20, 50],
+            #     }
+            #     scoring = [
+            #         "accuracy",
+            #         "f1_weighted",
+            #         "precision_weighted",
+            #         "recall_weighted",
+            #     ]
+            #     search = GridSearchCV(
+            #         pipeline,
+            #         param_grid=param_grid,
+            #         n_jobs=1,
+            #         cv=cv_inner,
+            #         scoring=scoring,
+            #         refit="f1_weighted",
+            #     )
 
             cv = KFold(n_splits=test_split_ratio,
                     shuffle=True, random_state=random_state)
 
-            if with_grid is True:
-                f1 = cross_val_score(
-                search, features, target, scoring="f1_weighted", cv=cv, n_jobs=1).mean()
-                mlflow.log_param("max_iter", search.best_params_["max_iter"])
-                mlflow.log_param("logreg_c", search.best_params_["C"])
-                search.fit(features, target)
-            else:
-                f1 = cross_val_score(
-                    pipeline, features, target, scoring="f1_weighted", cv=cv, n_jobs=1).mean()
-                pipeline.fit(features, target)
+            # if with_grid is True:
+            #     f1 = cross_val_score(
+            #     search, features, target, scoring="f1_weighted", cv=cv, n_jobs=1).mean()
+            #     mlflow.log_param("max_iter", search.best_params_["max_iter"])
+            #     mlflow.log_param("logreg_c", search.best_params_["C"])
+            #     search.fit(features, target)
+            # else:
+            f1 = cross_val_score(
+                pipeline, features, target, scoring="f1_weighted", cv=cv, n_jobs=1).mean()
+            pipeline.fit(features, target)
             
             if with_feature_selection == 0:
                 mlflow.log_param("with_feature_selection", "None")
@@ -166,10 +167,15 @@ def train(
             mlflow.log_metric("f1-score", f1)
             click.echo(f"f1-score: {f1}.")
 
+            accuracy = accuracy_score(target, pipeline.predict(features))
+            mlflow.log_metric("accuracy", accuracy)
+            click.echo(f"accuracy: {accuracy}.")
+
             dump(pipeline, save_model_path)
 
             click.echo(f"Model is saved to {save_model_path}.")
         
+
         if model_selector == 2:
             pipeline = create_pipeline_RandomForest(
                 with_scaler,
@@ -181,41 +187,41 @@ def train(
 
             mlflow.log_param("Selected Model", "Random Forest Classifier")
 
-            if with_grid is False:
-                mlflow.log_param("n_estimators", n_estimators)
+            # if with_grid is False:
+            mlflow.log_param("n_estimators", n_estimators)
 
-            if with_grid is True:
-                cv_inner = KFold(n_splits=3, shuffle=True, random_state=1)
-                param_grid = {
-                    "classifier__n_estimators": [10, 100, 150, 200],
-                    "classifier__max_features": ["auto", "sqrt", "log2"],
-                    "classifier__max_depth": [2, 4, 5, 6, None],
-                    "classifier__criterion": ["gini", "entropy"],
-                }
-                scoring = [
-                    "accuracy",
-                    "f1_weighted",
-                    "precision_weighted",
-                    "recall_weighted",
-                ]
-                search = GridSearchCV(
-                    pipeline,
-                    param_grid=param_grid,
-                    n_jobs=1,
-                    cv=cv_inner,
-                    scoring=scoring,
-                    refit="f1_weighted",
-                )
+            # if with_grid is True:
+            #     cv_inner = KFold(n_splits=3, shuffle=True, random_state=1)
+            #     param_grid = {
+            #         "classifier__n_estimators": [10, 20, 50],
+            #         "classifier__max_features": ["auto", "sqrt", "log2"],
+            #         "classifier__max_depth": [2, 4, 5, 6, None],
+            #         "classifier__criterion": ["gini", "entropy"],
+            #     }
+            #     scoring = [
+            #         "accuracy",
+            #         "f1_weighted",
+            #         "precision_weighted",
+            #         "recall_weighted",
+            #     ]
+            #     search = GridSearchCV(
+            #         pipeline,
+            #         param_grid=param_grid,
+            #         n_jobs=1,
+            #         cv=cv_inner,
+            #         scoring=scoring,
+            #         refit="f1_weighted",
+            #     )
 
             cv = KFold(n_splits=test_split_ratio, shuffle=True, random_state=random_state)
 
-            if with_grid is True:
-                f1 = cross_val_score(search, features, target, scoring="f1_weighted", cv=cv, n_jobs=1).mean()
-                mlflow.log_param("n_estimators", search.best_params_["n_estimators"])
-                search.fit(features, target)
-            else:
-                f1 = cross_val_score(pipeline, features, target, scoring="f1_weighted", cv=cv, n_jobs=1).mean()
-                pipeline.fit(features, target)
+            # if with_grid is True:
+            #     f1 = cross_val_score(search, features, target, scoring="f1_weighted", cv=cv, n_jobs=1).mean()
+            #     mlflow.log_param("n_estimators", search.best_params_["n_estimators"])
+            #     search.fit(features, target)
+            # else:
+            f1 = cross_val_score(pipeline, features, target, scoring="f1_weighted", cv=cv, n_jobs=1).mean()
+            pipeline.fit(features, target)
 
             if with_feature_selection == 0:
                 mlflow.log_param("with_feature_selection", "None")
@@ -231,6 +237,10 @@ def train(
 
             mlflow.log_metric("f1-score", f1)
             click.echo(f"f1-score: {f1}.")
+
+            accuracy = accuracy_score(target, pipeline.predict(features))
+            mlflow.log_metric("accuracy", accuracy)
+            click.echo(f"accuracy: {accuracy}.")
 
             dump(pipeline, save_model_path)
 
